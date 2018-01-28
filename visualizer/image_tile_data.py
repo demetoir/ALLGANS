399887c91007f5b71918a50dc753a5c1e423a0ac
demetoir-ubuntu <wnsqlehlswk@naver.com>
2018-01-29 04:01:08 +0900

    [fix] add and fix util func
    add
    np_img_NCWH_to_NHWC
    np_img_uint8_to_float32
    np_img_float32_to_uint8
    
    fix
    np_img_to_PIL_img

--- visualizer/image_tile_data.py
+++ visualizer/image_tile_data.py
@@ -8,6 +8,7 @@ import os
 class image_tile_data(AbstractVisualizer):
     def task(self, sess=None, iter_num=None, model=None, dataset=None):
         sample_imgs = dataset.next_batch(model.batch_size, batch_keys=[BATCH_KEY_TRAIN_X])
+        sample_imgs = np_img_float_to_uint8(sample_imgs)
 
         img_path = os.path.join(self.visualizer_path, '{}.png'.format(str(iter_num).zfill(5)))
         tile = np_img_to_tile(sample_imgs, column_size=8)
