7eacd18d0ffc27293aa5d02b3c2b74ca1826e856
demetoir-ubuntu <wnsqlehlswk@naver.com>
2018-01-29 09:26:10 +0900

    [fix] CIFAR10.py apply fixed AbstractDataset
    apply fixed AbstractDataset
    make some attrs to protect

--- data_handler/CIFAR10.py
+++ data_handler/CIFAR10.py
@@ -3,84 +3,61 @@ from glob import glob
 from dict_keys.dataset_batch_keys import *
 import os
 import pickle
-import requests
-import tarfile
 
 from util.util import extract_tar, download_data
 
 
 class CIFAR10(AbstractDataset):
-    PATTERN_TRAIN_FILE = "data_batch_*"
-    PATTERN_TEST_FILE = "test_batch"
-    PKCL_KEY_TRAIN_DATA = b"data"
-    PKCL_KEY_TRAIN_LABEL = b"labels"
-    PKCL_KEY_TEST_DATA = b"data"
-    PKCL_KEY_TEST_LABEL = b"labels"
+    _PATTERN_TRAIN_FILE = "data_batch_*"
+    _PATTERN_TEST_FILE = "test_batch"
+    _PKCL_KEY_TRAIN_DATA = b"data"
+    _PKCL_KEY_TRAIN_LABEL = b"labels"
+    _PKCL_KEY_TEST_DATA = b"data"
+    _PKCL_KEY_TEST_LABEL = b"labels"
     LABEL_SIZE = 10
-    SOURCE_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-    SOURCE_FILE = 'cifar-10-python.tar.gz'
 
     def __init__(self, preprocess=None, batch_after_task=None):
         super().__init__(preprocess, batch_after_task)
         self.batch_keys = [BATCH_KEY_TRAIN_X, BATCH_KEY_TRAIN_LABEL, BATCH_KEY_TEST_X, BATCH_KEY_TEST_LABEL]
+        self.SOURCE_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+        self.SOURCE_FILE = 'cifar-10-python.tar.gz'
+        self._data_files = [
+            "data_batch_1",
+            "data_batch_2",
+            "data_batch_3",
+            "data_batch_4",
+            "data_batch_5",
+            "test_batch",
+            "batches.meta"
+        ]
 
     def load(self, path, limit=None):
-        try:
-            os.makedirs(path)
-            self.log('create dir %s' % path)
-        except FileExistsError:
-            pass
 
-        # TODO hack better way to inspect data file
-        download = False
-        files = glob(os.path.join(path, self.PATTERN_TRAIN_FILE))
-        if len(files) != 5:
-            download = True
+        # load train data
+        files = glob(os.path.join(path, self._PATTERN_TRAIN_FILE))
+        files.sort()
+        for file in files:
+            with open(file, 'rb') as fo:
+                dict_ = pickle.load(fo, encoding='bytes')
 
-        files = glob(os.path.join(path, self.PATTERN_TEST_FILE))
-        if len(files) != 1:
-            download = True
+            x = dict_[self._PKCL_KEY_TRAIN_DATA]
+            self._append_data(BATCH_KEY_TRAIN_X, x)
 
-        if download:
-            head, _ = os.path.split(path)
-            download_file = os.path.join(head, self.SOURCE_FILE)
-            self.log('download %s at %s ' % (self.SOURCE_FILE, download_file))
-            download_data(self.SOURCE_URL, download_file)
+            label = dict_[self._PKCL_KEY_TRAIN_LABEL]
+            self._append_data(BATCH_KEY_TRAIN_LABEL, label)
 
-            self.log("extract %s at %s" % (self.SOURCE_FILE, head))
-            extract_tar(download_file, head)
+        # load test data
+        files = glob(os.path.join(path, self._PATTERN_TEST_FILE))
+        files.sort()
+        for file in files:
+            with open(file, 'rb') as fo:
+                dict_ = pickle.load(fo, encoding='bytes')
 
-        try:
-            # load train data
-            files = glob(os.path.join(path, self.PATTERN_TRAIN_FILE))
-            files.sort()
-            for file in files:
-                with open(file, 'rb') as fo:
-                    dict_ = pickle.load(fo, encoding='bytes')
+            x = dict_[self._PKCL_KEY_TEST_DATA]
+            self._append_data(BATCH_KEY_TEST_X, x)
 
-                x = dict_[self.PKCL_KEY_TRAIN_DATA]
-                self._append_data(BATCH_KEY_TRAIN_X, x)
-
-                label = dict_[self.PKCL_KEY_TRAIN_LABEL]
-                self._append_data(BATCH_KEY_TRAIN_LABEL, label)
-
-            # load test data
-            files = glob(os.path.join(path, self.PATTERN_TEST_FILE))
-            files.sort()
-            for file in files:
-                with open(file, 'rb') as fo:
-                    dict_ = pickle.load(fo, encoding='bytes')
-
-                x = dict_[self.PKCL_KEY_TEST_DATA]
-                self._append_data(BATCH_KEY_TEST_X, x)
-
-                label = dict_[self.PKCL_KEY_TEST_LABEL]
-                self._append_data(BATCH_KEY_TEST_LABEL, label)
-
-        except Exception as e:
-            self.log(e)
-
-        super().load(path, limit)
+            label = dict_[self._PKCL_KEY_TEST_LABEL]
+            self._append_data(BATCH_KEY_TEST_LABEL, label)
 
     def save(self):
         raise NotImplementedError
