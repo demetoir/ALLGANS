312bd0577dbf0a79aed050c2a98a0f0dc7b81036
demetoir <wnsqlehlswk@naver.com>
2018-02-06 04:32:16 +0900

    [refactoring] InstanceManagerHelper.py
    rename function
    gen_model_and_train -> build_and_train
    test_models -> build_models
    load_model_and_train -> load_and_train
    load_model_and_sample -> load_and_sampling

--- workbench/InstanceManagerHelper.py
+++ workbench/InstanceManagerHelper.py
@@ -3,36 +3,37 @@ from InstanceManger import InstanceManager
 
 class InstanceManagerHelper:
     @staticmethod
-    def gen_model_and_train(model=None, input_shapes=None, dataset=None, visualizers=None, env_path=None, epoch_time=50,
-                            check_point_interval_per_iter=5000):
+    def build_and_train(model=None, input_shapes=None, dataset=None, visualizers=None, env_path=None, epoch_time=50,
+                        check_point_interval_per_iter=5000):
         manager = InstanceManager(env_path)
-        manager.gen_instance(model, input_shapes)
+        metadata_path = manager.build_instance(model)
+        manager.load_instance(metadata_path, input_shapes)
         manager.load_visualizer(visualizers)
 
         manager.open_tensorboard()
-        manager.train_model(dataset, epoch_time, check_point_interval_per_iter)
+        manager.train_instance(dataset, epoch_time, check_point_interval_per_iter)
         manager.close_tensorboard()
         del manager
 
     @staticmethod
-    def test_models(model_list=None, input_shapes=None, env_setting=None):
+    def build_models(model_list=None, input_shapes=None, env_setting=None):
         for model in model_list:
             manager = InstanceManager(env_setting)
-            manager.gen_instance(model, input_shapes)
+            manager.build_instance(model, input_shapes)
             del manager
 
     @staticmethod
-    def load_model_and_train(input_shapes=None, visualizers=None, metadata_path=None, env_setting=None, dataset=None,
-                             epoch_time=50, check_point_interval_per_iter=5000):
+    def load_and_train(input_shapes=None, visualizers=None, metadata_path=None, env_setting=None, dataset=None,
+                       epoch_time=50, check_point_interval_per_iter=5000):
         manager = None
         try:
             manager = InstanceManager(env_setting)
-            manager.load_model(metadata_path=metadata_path, input_shapes=input_shapes)
+            manager.load_instance(instance_path=metadata_path, input_shapes=input_shapes)
             manager.load_visualizer(visualizers)
 
             manager.open_tensorboard()
 
-            manager.train_model(dataset, epoch_time, check_point_interval_per_iter, is_restore=True)
+            manager.train_instance(dataset, epoch_time, check_point_interval_per_iter, is_restore=True)
         except Exception as e:
             print(e)
         finally:
@@ -40,8 +41,8 @@ class InstanceManagerHelper:
             del manager
 
     @staticmethod
-    def load_model_and_sample(input_shapes=None, visualizers=None, metadata_path=None, env_setting=None):
+    def load_and_sampling(input_shapes=None, visualizers=None, metadata_path=None, env_setting=None):
         manager = InstanceManager(env_setting)
-        manager.load_model(metadata_path=metadata_path, input_shapes=input_shapes)
+        manager.load_instance(instance_path=metadata_path, input_shapes=input_shapes)
         manager.load_visualizer(visualizers)
-        manager.sample_model(is_restore=True)
+        manager.sampling_instance(is_restore=True)
