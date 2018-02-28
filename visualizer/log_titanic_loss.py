b796e166eb955b606e4b89152faa84142f3608a6
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-02-28 23:59:22 +0900

    add log_titanic_loss.py

---
+++ visualizer/log_titanic_loss.py
@@ -0,0 +1,23 @@
+from dict_keys.dataset_batch_keys import *
+from visualizer.AbstractVisualizer import AbstractVisualizer
+
+
+class log_titanic_loss(AbstractVisualizer):
+    """visualize log of classifier's loss"""
+
+    def task(self, sess=None, iter_num=None, model=None, dataset=None):
+        batch_xs, batch_labels = dataset.next_batch(model.batch_size,
+                                                    batch_keys=[BATCH_KEY_TRAIN_X, BATCH_KEY_TRAIN_LABEL])
+        loss, train_acc, global_step = sess.run([model.loss_mean, model.batch_acc, model.global_step],
+                                                feed_dict={model.X: batch_xs, model.label: batch_labels})
+
+        # batch_xs, batch_labels = dataset.next_batch(model.batch_size,
+        #                                             batch_keys=[BATCH_KEY_TEST_X, BATCH_KEY_TEST_LABEL])
+        # test_acc = sess.run(model.batch_acc, feed_dict={model.X: batch_xs, model.label: batch_labels})
+
+        self.log(
+            'global_step : %04d ' % global_step,
+            'loss: {:.4} '.format(loss),
+            'train acc: {:.4} '.format(train_acc),
+            # 'test acc: {:.4} '.format(test_acc),
+        )
