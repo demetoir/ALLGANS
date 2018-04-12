9c7275199d40fa208aba3b5d5f89710971966b13
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-04-12 20:46:50 +0900

    extract func get_total_acc

--- visualizer/log_titanic_loss.py
+++ visualizer/log_titanic_loss.py
@@ -1,5 +1,26 @@
 from dict_keys.dataset_batch_keys import *
 from visualizer.AbstractVisualizer import AbstractVisualizer
+import numpy as np
+
+
+def get_total_acc(sess, model, next_batch_func, batch_loop, total_size):
+    acc_list = []
+    for i in range(batch_loop):
+        batch_xs, batch_labels = next_batch_func(
+            model.batch_size,
+            batch_keys=[BK_X, BK_LABEL]
+        )
+        acc_list += [sess.run(
+            model.batch_acc,
+            feed_dict={
+                model.X: batch_xs,
+                model.label: batch_labels,
+                model.dropout_rate: 1
+            }
+        )]
+    acc = np.concatenate(acc_list, axis=0)[:total_size]
+    acc = sum(acc) / total_size
+    return acc
 
 
 class log_titanic_loss(AbstractVisualizer):
@@ -19,39 +40,14 @@ class log_titanic_loss(AbstractVisualizer):
             }
         )
 
-        train_acc = 0.0
-        for i in range(dataset.train_set.data_size // model.batch_size + 1):
-            batch_xs, batch_labels = dataset.train_set.next_batch(
-                model.batch_size,
-                batch_keys=[BK_X, BK_LABEL]
-            )
-            acc = sess.run(
-                model.batch_acc,
-                feed_dict={
-                    model.X: batch_xs,
-                    model.label: batch_labels,
-                    model.dropout_rate: 1
-                }
-            )
-            train_acc += acc
-        train_acc /= (dataset.train_set.data_size // model.batch_size + 1)
+        batch_loop = dataset.train_set.data_size // model.batch_size + 1
+        next_batch = dataset.train_set.next_batch
+        train_acc = get_total_acc(sess, model, next_batch, batch_loop, dataset.train_set.data_size)
 
-        valid_acc = 0.0
-        for i in range(dataset.validation_set.data_size // model.batch_size + 1):
-            batch_xs, batch_labels = dataset.validation_set.next_batch(
-                model.batch_size,
-                batch_keys=[BK_X, BK_LABEL]
-            )
-            acc = sess.run(
-                model.batch_acc,
-                feed_dict={
-                    model.X: batch_xs,
-                    model.label: batch_labels,
-                    model.dropout_rate: 1
-                }
-            )
-            valid_acc += acc
-        valid_acc /= (dataset.validation_set.data_size // model.batch_size + 1)
+        batch_loop = dataset.validation_set.data_size // model.batch_size + 1
+        next_batch = dataset.validation_set.next_batch
+        dataset.validation_set.reset_cursor()
+        valid_acc = get_total_acc(sess, model, next_batch, batch_loop, dataset.validation_set.data_size)
 
         self.log(
             'global_step : %04d ' % global_step,
