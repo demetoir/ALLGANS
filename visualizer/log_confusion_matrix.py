4b43f79a33101e0ffc5180dbcb86f077cfa056d8
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-04-13 18:18:07 +0900

    better log_confusion_matrix

--- visualizer/log_confusion_matrix.py
+++ visualizer/log_confusion_matrix.py
@@ -1,6 +1,7 @@
 from dict_keys.dataset_batch_keys import *
 from visualizer.AbstractVisualizer import AbstractVisualizer
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class log_confusion_matrix(AbstractVisualizer):
@@ -9,10 +10,15 @@ class log_confusion_matrix(AbstractVisualizer):
     def task(self, sess=None, iter_num=None, model=None, dataset=None):
         matrix = np.zeros([dataset.LABEL_SIZE, dataset.LABEL_SIZE], dtype=np.int32)
 
-        for _ in range(1):
-            batch_x, batch_labels = dataset.validation_set.next_batch(model.batch_size, batch_keys=[BK_X, BK_LABEL])
-            predict_labels, true_labels = sess.run(
-                [model.predict_index, model.label_index],
+        dataset.validation_set.reset_cursor()
+        batch_loop = dataset.validation_set.data_size // model.batch_size + 1
+        hs_list = []
+
+        for _ in range(batch_loop):
+            batch_x, batch_labels, ids = dataset.validation_set.next_batch(model.batch_size,
+                                                                           batch_keys=[BK_X, BK_LABEL, "PassengerId"])
+            predict_labels, true_labels, hs = sess.run(
+                [model.predict_index, model.label_index, model.h],
                 feed_dict={
                     model.X: batch_x,
                     model.label: batch_labels,
@@ -20,9 +26,22 @@ class log_confusion_matrix(AbstractVisualizer):
                 }
             )
 
-            for true_label, predict_label in zip(true_labels, predict_labels):
+            for true_label, predict_label, h, id_ in zip(true_labels, predict_labels, hs, ids):
                 matrix[int(true_label - 1)][int(predict_label - 1)] += 1
-                # if int(true_label - 1) != int(predict_label - 1):
+                if true_label != predict_label:
+                    hs_list += [h]
+                    # self.log("id: %s, h: %s, true label: %s" % (id_, h, true_label))
+        x, y = zip(*hs_list)
+        fig, axs = plt.subplots(1, 2, tight_layout=True)
+        axs[0].hist(x)
+        axs[1].hist(y)
+        # plt.show()
 
         # TODO better look
-        self.log("confusion matrix total %d samples\n" % (model.batch_size * 1) + str(matrix))
+        msg = "confusion matrix total %d samples\n%s\n\n%s" \
+              % (model.batch_size * batch_loop,
+                 str(np.round(matrix, decimals=4)),
+                 str(np.round(matrix / (model.batch_size * batch_loop), decimals=4))
+                 )
+
+        self.log(msg)
