ec2010ac1686b8cfcf2fd1a59556d5730ef4c1c0
demetoir <wnsqlehlswk@naver.com>
2018-04-22 03:31:12 +0900

    temp update sklearn_toolkit.py

--- workbench/sklearn_toolkit.py
+++ workbench/sklearn_toolkit.py
@@ -13,53 +13,134 @@ NO_CONVERT = (
 )
 
 
-class BaseSklearnClassfier:
-    model_Ys_type = None
+def reformat_Ys(Ys, model_Ys_type, Ys_type=None):
+    def get_Ys_type(Ys):
+        shape = Ys.shape
+        if len(shape) == 1:
+            _type = "index"
+        elif len(shape) == 2:
+            _type = "onehot"
+        else:
+            _type = "invalid"
 
+        return _type
+
+    if Ys_type is None:
+        Ys_type = get_Ys_type(Ys)
+
+    convert_type = (Ys_type, model_Ys_type)
+
+    if convert_type == ONEHOT_TO_INDEX:
+        Ys = np_onehot_to_index(Ys)
+    elif convert_type == INDEX_TO_ONEHOT:
+        Ys = np_index_to_onehot(Ys)
+    elif convert_type in NO_CONVERT:
+        pass
+    else:
+        raise TypeError("Ys convert type Error Ys_type=%s, model_Ys_type=%s" % (Ys_type, model_Ys_type))
+
+    return Ys
+
+
+def print_dict(d):
+    for k in d:
+        print("'%s' : %s," % (k, d[k]))
+
+
+class BaseSklearn:
     def __str__(self):
         return self.__class__.__name__
 
     def __init__(self, *args, **kwargs):
-        self.model = None
-
-    def reformat_Ys(self, Ys, Ys_type=None):
-        def get_Ys_type(Ys):
-            shape = Ys.shape
-            if len(shape) == 2:
-                _type = "index"
-            elif len(shape) == 3:
-                _type = "onehot"
-            else:
-                _type = "invalid"
-
-            return _type
-
-        if Ys_type is None:
-            Ys_type = get_Ys_type(Ys)
-
-        convert_type = (Ys_type, self.model_Ys_type)
-
-        if convert_type == ONEHOT_TO_INDEX:
-            Ys = np_onehot_to_index(Ys)
-        elif convert_type == INDEX_TO_ONEHOT:
-            Ys = np_index_to_onehot(Ys)
-        elif convert_type in NO_CONVERT:
-            pass
-        else:
-            raise TypeError("Ys convert type Error Ys_type=%s, model_Ys_type=%s" % (Ys_type, self.model_Ys_type))
-
-        return Ys
+        pass
 
     def fit(self, Xs, Ys, Ys_type=None):
-        self.model.fit(Xs, self.reformat_Ys(Ys, Ys_type=Ys_type))
+        pass
+
+    def predict(self, Xs):
+        pass
+
+    def score(self, Xs, Ys, Ys_type):
+        pass
+
+    def proba(self, Xs, transpose_shape=True):
+        pass
+
+    def get_params(self, deep=True):
+        pass
+
+    def set_params(self, **params):
+        pass
+
+
+class ParamGridSearch(BaseSklearn):
+
+    def __init__(self, estimator, param_grid, **kwargs):
+        super().__init__(estimator, param_grid, **kwargs)
+        self.estimator = estimator
+        self.param_grid = param_grid
+
+        from sklearn.model_selection import GridSearchCV as _GridSearchCV
+        self.param_search = _GridSearchCV(estimator, param_grid, **kwargs)
+        del _GridSearchCV
+
+        # print_dict(self.param_search.__dict__)
+
+    @property
+    def cv_results_(self):
+        return self.param_search.cv_results_
+
+    @property
+    def best_estimator(self):
+        return self.param_search.best_estimator_
+
+    @property
+    def best_params_(self):
+        return self.param_search.best_params_
+
+    @property
+    def best_score_(self):
+        return self.param_search.best_score_
+
+    def fit(self, Xs, Ys, Ys_type=None):
+        Ys = reformat_Ys(Ys, self.estimator.model_Ys_type, Ys_type=Ys_type)
+        self.param_search.fit(Xs, Ys)
+
+    def predict(self, Xs):
+        super().predict(Xs)
+
+    def score(self, Xs, Ys, Ys_type):
+        super().score(Xs, Ys, Ys_type)
+
+    def proba(self, Xs, transpose_shape=True):
+        super().proba(Xs, transpose_shape)
+
+    def get_params(self, deep=True):
+        super().get_params(deep)
+        return self.param_search.get_params(deep=deep)
+
+    def set_params(self, **params):
+        super().set_params(**params)
+        return self.param_search.set_params(**params)
+
+
+class BaseSklearnClassifier(BaseSklearn):
+    model_Ys_type = None
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.model = None
+
+    def fit(self, Xs, Ys, Ys_type=None):
+        self.model.fit(Xs, reformat_Ys(Ys, self.model_Ys_type, Ys_type=Ys_type))
 
     def predict(self, Xs):
         return self.model.predict(Xs)
 
-    def acc(self, Xs, Ys, Ys_type=True):
-        return self.model.score(Xs, self.reformat_Ys(Ys, Ys_type=Ys_type))
+    def score(self, Xs, Ys, Ys_type=None):
+        return self.model.score(Xs, reformat_Ys(Ys, self.model_Ys_type, Ys_type=Ys_type))
 
-    def prob(self, Xs, transpose_shape=True):
+    def proba(self, Xs, transpose_shape=True):
         """
         if multi label than output shape == (class, sample, prob)
         need to transpose shape to (sample, class, prob)
@@ -75,155 +156,180 @@ class BaseSklearnClassfier:
 
         return probs
 
+    def get_params(self, deep=True):
+        return self.model.get_params(deep=deep)
 
-class DecisionTree(BaseSklearnClassfier):
-    """
-    sklearn base DecisionTreeClassifier
-    """
+    def set_params(self, **params):
+        return self.model.set_params(**params)
+
+
+class MLP(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_ONEHOT
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        from sklearn.tree import DecisionTreeClassifier as _d_tree
-        self.model = _d_tree(*args, **kwargs)
-        del _d_tree
-
-
-class MLP(BaseSklearnClassfier):
-    model_Ys_type = YS_TYPE_ONEHOT
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.neural_network import MLPClassifier as _MLP
         self.model = _MLP(alpha=1)
         del _MLP
 
 
-class Gaussian_NB(BaseSklearnClassfier):
+class Gaussian_NB(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.naive_bayes import GaussianNB as _GaussianNB
-        self.model = _GaussianNB()
+        self.model = _GaussianNB(**params)
         del _GaussianNB
 
 
-class Bernoulli_NB(BaseSklearnClassfier):
+class Bernoulli_NB(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.naive_bayes import BernoulliNB as _BernoulliNB
-        self.model = _BernoulliNB()
+        self.model = _BernoulliNB(**params)
         del _BernoulliNB
 
 
-class Multinomial_NB(BaseSklearnClassfier):
+class Multinomial_NB(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.naive_bayes import MultinomialNB as _MultinomialNB
-        self.model = _MultinomialNB()
+        self.model = _MultinomialNB(**params)
         del _MultinomialNB
 
 
-class QDA(BaseSklearnClassfier):
+class QDA(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as _QDA
-        self.model = _QDA()
+        self.model = _QDA(**params)
         del _QDA
 
 
-class RandomForest(BaseSklearnClassfier):
+class DecisionTree(BaseSklearnClassifier):
+    """
+    sklearn base DecisionTreeClassifier
+    """
     model_Ys_type = YS_TYPE_ONEHOT
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
+        from sklearn.tree import DecisionTreeClassifier as _DecisionTreeClassifier
+        self.model = _DecisionTreeClassifier(**params)
+        del _DecisionTreeClassifier
+
+
+class RandomForest(BaseSklearnClassifier):
+    model_Ys_type = YS_TYPE_ONEHOT
+
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.ensemble import RandomForestClassifier as _RandomForestClassifier
-        self.model = _RandomForestClassifier(
-            max_depth=5,
-            n_estimators=10,
-            max_features=1
-        )
+        self.model = _RandomForestClassifier(**params)
         del _RandomForestClassifier
 
 
-class AdaBoost(BaseSklearnClassfier):
+class ExtraTrees(BaseSklearnClassifier):
+    model_Ys_type = YS_TYPE_ONEHOT
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        from sklearn.ensemble import ExtraTreesClassifier as _ExtraTreesClassifier
+        self.model = _ExtraTreesClassifier(**params)
+        del _ExtraTreesClassifier
+
+
+class AdaBoost(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.ensemble import AdaBoostClassifier as _AdaBoostClassifier
-        self.model = _AdaBoostClassifier()
+        self.model = _AdaBoostClassifier(**params)
         del _AdaBoostClassifier
 
 
-class KNeighbors(BaseSklearnClassfier):
+class GradientBoosting(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
+        from sklearn.ensemble import GradientBoostingClassifier as _GradientBoostingClassifier
+        self.model = _GradientBoostingClassifier(**params)
+        del _GradientBoostingClassifier
+
+
+class KNeighbors(BaseSklearnClassifier):
+    model_Ys_type = YS_TYPE_INDEX
+
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.neighbors import KNeighborsClassifier as _KNeighborsClassifier
-        self.model = sklearn.neighbors.KNeighborsClassifier()
+        self.model = sklearn.neighbors.KNeighborsClassifier(**params)
         del _KNeighborsClassifier
 
 
-class Linear_SVM(BaseSklearnClassfier):
+class Linear_SVM(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.svm import SVC as _SVC
+        # TODO **params
         self.model = _SVC(kernel="linear", C=0.025)
         del _SVC
 
-    def prob(self, Xs, transpose_shape=True):
+    def proba(self, Xs, transpose_shape=True):
         print("""linear_SVM does not have attr "prob" """)
         return None
 
 
-class RBF_SVM(BaseSklearnClassfier):
+class RBF_SVM(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.svm import SVC as _SVC
+        # TODO **params
         self.model = _SVC(gamma=2, C=1)
         del _SVC
 
-    def prob(self, Xs, transpose_shape=True):
+    def proba(self, Xs, transpose_shape=True):
         print("""linear_SVM does not have attr "prob" """)
         return None
 
 
-class GaussianProcess(BaseSklearnClassfier):
+class GaussianProcess(BaseSklearnClassifier):
     model_Ys_type = YS_TYPE_INDEX
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **params):
+        super().__init__(**params)
         from sklearn.gaussian_process import GaussianProcessClassifier as _GaussianProcessClassifier
         from sklearn.gaussian_process.kernels import RBF as _RBF
+        # TODO **params
         self.model = _GaussianProcessClassifier(1.0 * _RBF(1.0))
         del _RBF
         del _GaussianProcessClassifier
 
 
-classifiers = [
-    Gaussian_NB,
-    Bernoulli_NB,
-    Multinomial_NB,
-    DecisionTree,
-    RandomForest,
-    AdaBoost,
-    MLP,
-    QDA,
-    KNeighbors,
-    Linear_SVM,
-    RBF_SVM,
-    GaussianProcess,
-]
+class SGD(BaseSklearnClassifier):
+    model_Ys_type = YS_TYPE_INDEX
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        from sklearn.linear_model import SGDClassifier as _SGDClassifier
+        self.model = _SGDClassifier(**params)
+        del _SGDClassifier
+
+    def proba(self, Xs, transpose_shape=True):
+        # todo
+        return None
+        # return super().proba(Xs, transpose_shape)
+
