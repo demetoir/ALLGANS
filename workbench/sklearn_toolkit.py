8ac81388ee5506ae2d7044351d3c99884e3d5d0e
demetoir <wnsqlehlswk@naver.com>
2018-05-04 01:33:19 +0900

    update LightGBM

--- workbench/sklearn_toolkit.py
+++ workbench/sklearn_toolkit.py
@@ -869,6 +869,66 @@ class XGBoost(BaseSklearnClassifier):
         del XGB
 
 
+class LightGBM(BaseSklearnClassifier):
+    model_Ys_type = YS_TYPE_INDEX
+    tuning_grid = {
+        'num_leaves': [4, 8, 16, 32],
+        'min_child_samples': [4, 8, 16, 32],
+        'max_depth': [2, 4, 6, 8],
+        'colsample_bytree': [i / 10.0 for i in range(2, 10 + 1, 2)],
+        'subsample': [i / 10.0 for i in range(2, 10 + 1, 2)],
+        # 'max_bin': [64, 128],
+        # 'top_k': [8, 16, 32],
+    }
+    tuning_params = {
+
+    }
+    param = {
+        'learning_rate': 0.1,
+        # 'num_boost_round': 100,
+        # ???
+        'max_delta_step': 0,
+        'min_split_gain': 0,
+
+        # device option
+        # 'device': 'gpu',
+        'device': 'cpu',
+
+        # dart only option
+        'drop_rate': 0.1,
+        'skip_drop': 0.5,
+        'max_drop': 50,
+        'uniform_drop': False,
+        'xgboost_dart_mode': False,
+        'drop_seed': 4,
+
+        # goss only option
+        'other_rate': 0.1,
+        'min_data_per_group': 100,
+
+        # default value
+        'feature_fraction_seed': 2,
+        'bagging_seed': 3,
+        # 'early_stopping_round': 0,
+        'reg_alpha': 0,
+        'reg_lambda': 0,
+        'max_cat_threshold': 32,
+        'cat_smooth': 10,
+        'cat_l2': 10,
+        'max_cat_to_onehot': 4,
+        'verbose': -1
+
+    }
+
+    def __init__(self, **params):
+        super().__init__(**params)
+
+        import lightgbm as lgbm
+        params.update(self.param)
+        self.model = lgbm.LGBMClassifier(**params)
+        del lgbm
+
+
 class ClassifierPack(BaseClass):
     SGD = SGD
     Gaussian_NB = Gaussian_NB
@@ -886,6 +946,7 @@ class ClassifierPack(BaseClass):
     RBF_SVM = RBF_SVM
     GaussianProcess = GaussianProcess
     XGBoost = XGBoost
+    LightGBM = LightGBM
     clf_pack = [
         MLP,
         SGD,
@@ -903,6 +964,7 @@ class ClassifierPack(BaseClass):
         RBF_SVM,
         GaussianProcess,
         XGBoost,
+        LightGBM,
     ]
 
     def __init__(self):
