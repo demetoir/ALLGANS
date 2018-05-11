db58f568f09266b399a91f68fe49313bb1cc0e9e
demetoir <wnsqlehlswk@naver.com>
2018-05-11 09:09:56 +0900

    update bench_code

--- workbench/bench_code.py
+++ workbench/bench_code.py
@@ -1,10 +1,17 @@
+# -*- coding:utf-8 -*-
+import codecs
+import webbrowser
+import requests
+from bs4 import BeautifulSoup
+
 from InstanceManger import InstanceManager
+from util.Logger import StdoutOnlyLogger
 from workbench.InstanceManagerHelper import InstanceManagerHelper
 from DatasetLoader import DatasetLoader
 from VisualizerClassLoader import VisualizerClassLoader
 from ModelClassLoader import ModelClassLoader
 from workbench.sklearn_toolkit import *
-
+from data_handler.titanic import *
 from pprint import pprint
 
 # dataset, input_shapes = DatasetLoader().load_dataset("CIFAR10")
@@ -14,91 +21,171 @@ from pprint import pprint
 # dataset, input_shapes = DatasetLoader().load_dataset("Fashion_MNIST")
 # model = ModelClassLoader.load_model_class("GAN")
 
-from data_handler.titanic import *
+
+# print(built-in function) is not good for logging
+bprint = print
+logger = StdoutOnlyLogger()
+print = logger.get_log()
 
 
-def fit_and_test(model, dataset):
+def fit_and_test(model, train_Xs, train_Ys, test_Xs, test_Ys):
     instance = model()
     print(instance)
 
-    batch_xs, batch_labels = dataset.train_set.next_batch(
-        dataset.train_set.data_size,
-        batch_keys=[BK_X, BK_LABEL],
-    )
-    instance.fit(batch_xs, batch_labels, Ys_type="onehot")
+    instance.fit(train_Xs, train_Ys, Ys_type="onehot")
 
-    acc = instance.score(batch_xs, batch_labels, Ys_type="onehot")
+    acc = instance.score(train_Xs, train_Ys, Ys_type="onehot")
     print("train acc:", acc)
 
-    batch_xs, batch_labels = dataset.validation_set.next_batch(
-        dataset.validation_set.data_size,
-        batch_keys=[BK_X, BK_LABEL]
-    )
-    acc = instance.score(batch_xs, batch_labels, Ys_type="onehot")
+    acc = instance.score(test_Xs, test_Ys, Ys_type="onehot")
     print("valid acc:", acc)
 
     print("probs")
-    probs = instance.proba(batch_xs[:10], transpose_shape=False)
+    probs = instance.proba(test_Xs[:10], transpose_shape=False)
     print(probs)
 
     print("predict")
-    print(instance.predict(batch_xs[:10]))
+    print(instance.predict(test_Xs[:10]))
     print()
 
 
-def param_search(instance, dataset):
-    instance = instance()
-    pprint(instance)
-
-    optimizer = ParamOptimizer(instance, instance.tuning_grid)
-    train_batch_xs, train_batch_labels = dataset.train_set.next_batch(
-        dataset.train_set.data_size,
-        batch_keys=[BK_X, BK_LABEL],
-    )
-
-    test_batch_xs, test_batch_labels = dataset.validation_set.next_batch(
-        dataset.validation_set.data_size,
-        batch_keys=[BK_X, BK_LABEL],
-    )
-    optimizer.optimize(train_batch_xs, test_batch_xs,
-                       train_batch_labels, test_batch_labels)
-
-    optimizer.result_to_csv("./result.csv")
+def get_html(url):
+    _html = ""
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        _html = resp.text
+    return _html
 
 
-classifiers = [
-    # MLP,
-    # SGD,
-    # Gaussian_NB,
-    # Bernoulli_NB,
-    # Multinomial_NB,
-    DecisionTree,
-    # RandomForest,
-    # ExtraTrees,
-    # AdaBoost,
-    # GradientBoosting,
-    # QDA,
-    # KNeighbors,
-    # Linear_SVC,
-    # RBF_SVM,
-    # GaussianProcess,
-    # XGBoost
-]
+def deco_file_lines_job(func, in_file='input.txt', out_file='output.txt', encoding='UTF8'):
+    def wrapper():
+        with codecs.open(in_file, 'r', encoding=encoding) as f:
+            lines = [line for line in f.readlines()]
+
+        print("read '{}', {} lines".format(in_file, len(lines)))
+
+        lines = func(lines)
+
+        if lines is not None:
+            with codecs.open(out_file, 'w', encoding=encoding) as f:
+                f.writelines(lines)
+            print("write '{}', {} lines".format(out_file, len(lines)))
+
+    return wrapper
+
+
+def deco_file_str_job(func, in_file='input.txt', out_file='output.txt', encoding='UTF8'):
+    def wrapper():
+        with codecs.open(in_file, 'r', encoding=encoding) as f:
+            line = "".join([line for line in f.readlines()])
+
+        print("read '{}', {} lenght".format(in_file, len(line)))
+
+        line = func(line)
+
+        if line is not None:
+            with codecs.open(out_file, 'w', encoding=encoding) as f:
+                f.writelines(str(line))
+            print("write '{}', {} lenght".format(out_file, len(line)))
+
+    return wrapper
+
+
+def filter_str(s, filter_):
+    for del_char in filter_:
+        s = s.replace(del_char, '')
+    return s
+
+
+@deco_file_lines_job
+def filter_lines(lines):
+    out = []
+    for line in lines:
+        print("%s [ %s ]" % (len(line), line))
+        line = filter_str(line, """+-():?"':!~,‘’""")
+        # if len(line) == 2 or len(line) > 30 or '[' in line or '.' in line:
+        #     continue
+        out += [line]
+
+        print("%s [ %s ]" % (len(line), line))
+
+    out = set(out)
+    out = list(out)
+    out = sorted(out)
+    return out
+
+
+@deco_file_lines_job
+def open_chrome(lines):
+    batch_size = 5
+    for idx in range(0, len(lines), batch_size):
+        url_head = "https://www.google.co.kr/search?q=%s"
+        for token in lines[idx:idx + batch_size]:
+            url = url_head % (token)
+            print(idx, url)
+            webbrowser.open(url)
+
+        input("enter to open next batch")
+
+
+@deco_file_lines_job
+def get_google_cite_url(lines):
+    out = []
+    for line in lines:
+        # url = "https://www.google.co.kr/search?q=%s" + line
+        url = line
+
+        print(line)
+
+        html = get_html(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        cites = soup.find_all("cite")
+        print(cites[0].text)
+        out += [cites[0].text]
+
+
+@deco_file_str_job
+def filter_book_mark(html):
+    # print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    print(len(soup.find_all('a')))
+    out = []
+    for i in soup.find_all('a'):
+        # if i is None: continue
+        # if hasattr(i, 'icon'):
+        #     i['icon'] = None
+        #     i['ICON'] = None
+        # print()
+        out += [i['href']]
+        # print(a)
+    # print(soup)
+
+    out= sorted(out)
+    return "\n".join(out)
+    # return html
 
 
 def main():
-    import warnings
-    warnings.filterwarnings(module='sklearn*', action='ignore', category=DeprecationWarning)
-    dataset = DatasetLoader().load_dataset("titanic")
-    input_shapes = dataset.train_set.input_shapes
+    # open_chrome()
+    filter_book_mark()
 
-    clfpack = ClassifierPack()
-
-    # for clf in clfpack.clf_pack:
-    #     fit_and_test(clf, dataset)
-
-    for clf in classifiers:
-        param_search(clf, dataset)
+    # dataset = DatasetLoader().load_dataset("titanic")
+    # input_shapes = dataset.train_set.input_shapes
+    #
+    # clfpack = ClassifierPack()
+    # # for clf in classifiers:
+    # #     fit_and_test(clf, dataset)
+    #
+    # train_xs, train_labels = dataset.train_set.next_batch(
+    #     dataset.train_set.data_size,
+    #     batch_keys=[BK_X, BK_LABEL],
+    # )
+    # test_xs, test_labels = dataset.validation_set.next_batch(
+    #     dataset.validation_set.data_size,
+    #     batch_keys=[BK_X, BK_LABEL],
+    # )
+    # clfpack.param_search(train_xs, train_labels, test_xs, test_labels)
 
     # model = ModelClassLoader.load_model_class("TitanicModel")
     #
