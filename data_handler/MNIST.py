e98dc17b35ed792b48dc43813f82a9597401dedf
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-05-12 03:53:47 +0900

    remake MNIST handler
    change super class

--- data_handler/MNIST.py
+++ data_handler/MNIST.py
@@ -1,80 +1,76 @@
 from __future__ import division
 from tensorflow.examples.tutorials.mnist import input_data
-from data_handler.AbstractDataset import AbstractDataset, AbstractDatasetHelper
-from env_settting import MNIST_PATH
-from dict_keys.dataset_batch_keys import *
-from dict_keys.input_shape_keys import *
+from data_handler.BaseDataset import BaseDataset, DatasetCollection
 import numpy as np
 
+Xs = 'Xs'
+Xs_img = 'Xs_img'
+labels = 'labels'
+TRAIN_SIZE = 60000
+TEST_SIZE = 10000
+LABEL_SIZE = 10
 
-class MNIST(AbstractDataset):
-    TRAIN_SIZE = 60000
-    TEST_SIZE = 10000
-    LABEL_SIZE = 10
 
-    def __init__(self, preprocess=None, batch_after_task=None):
-        super().__init__(preprocess, batch_after_task)
-        self.batch_keys = [
-            BATCH_KEY_TRAIN_X,
-            BATCH_KEY_TRAIN_LABEL,
-            BATCH_KEY_TEST_X,
-            BATCH_KEY_TEST_LABEL
-        ]
-        def dummy():
-            pass
+class MNIST_train(BaseDataset):
+    SIZE = TRAIN_SIZE
+    LABEL_SIZE = LABEL_SIZE
+    Xs = Xs
+    Xs_img = Xs_img
+    labels = labels
+    BATCH_KEYS = [
+        Xs,
+        labels,
+        Xs_img
+    ]
 
-        self.before_load_task = dummy
+    def load(self, path, limit=None):
+        mnist = input_data.read_data_sets(path, one_hot=True)
+        self.data[Xs], self.data[labels] = mnist.train.next_batch(self.SIZE)
+
+    def save(self):
+        pass
+
+    def preprocess(self):
+        data = self.data[Xs]
+        shape = data.shape
+        data = np.reshape(data, [shape[0], 28, 28, 1])
+        self.data[Xs] = data
+
+
+class MNIST_test(BaseDataset):
+    SIZE = TEST_SIZE
+    LABEL_SIZE = LABEL_SIZE
+    Xs = Xs
+    Xs_img = Xs_img
+    labels = labels
+    BATCH_KEYS = [
+        Xs,
+        labels,
+        Xs_img
+    ]
 
     def load(self, path, limit=None):
         mnist = input_data.read_data_sets(path, one_hot=True)
 
-        train_x, train_label = mnist.train.next_batch(self.TRAIN_SIZE)
-        test_x, test_label = mnist.test.next_batch(self.TEST_SIZE)
-
-        self.data = {
-            BATCH_KEY_TRAIN_X: train_x,
-            BATCH_KEY_TRAIN_LABEL: train_label,
-            BATCH_KEY_TEST_X: test_x,
-            BATCH_KEY_TEST_LABEL: test_label
-        }
+        self.data[Xs], self.data[labels] = mnist.test.next_batch(self.SIZE)
 
     def save(self):
-        raise NotImplementedError
+        pass
 
-
-class MNISTHelper(AbstractDatasetHelper):
-    @staticmethod
-    def preprocess(dataset):
-        # original MNIST image size is 28*28 but need to resize 32*32
-        data = dataset.data[BATCH_KEY_TRAIN_X]
+    def preprocess(self):
+        data = self.data[Xs]
         shape = data.shape
-        data = np.reshape(data, [shape[0], 28, 28])
-        npad = ((0, 0), (2, 2), (2, 2))
-        data = np.pad(data, pad_width=npad, mode='constant', constant_values=0)
-        data = np.reshape(data, [shape[0], 32, 32, 1])
-        dataset.data[BATCH_KEY_TRAIN_X] = data
+        data = np.reshape(data, [shape[0], 28, 28, 1])
+        self.data[Xs] = data
 
-        data = dataset.data[BATCH_KEY_TEST_X]
-        shape = data.shape
-        data = np.reshape(data, [shape[0], 28, 28])
-        npad = ((0, 0), (2, 2), (2, 2))
-        data = np.pad(data, pad_width=npad, mode='constant', constant_values=0)
-        data = np.reshape(data, [shape[0], 32, 32, 1])
-        dataset.data[BATCH_KEY_TEST_X] = data
 
-    @staticmethod
-    def next_batch_task(batch):
-        x, label = batch[0], batch[1]
-        return x, label
+class MNIST(DatasetCollection):
+    def __init__(self):
+        super().__init__()
+        self.train_set = MNIST_train()
+        self.test_set = MNIST_test()
 
-    @staticmethod
-    def load_dataset(path, limit=None):
-        mnist_data = MNIST(preprocess=MNISTHelper.preprocess)
-        mnist_data.load(path, limit=limit)
-        input_shapes = {
-            INPUT_SHAPE_KEY_DATA_X: [32, 32, 1],
-            INPUT_SHAPE_KEY_LABEL: [10],
-            INPUT_SHAPE_KEY_LABEL_SIZE: 10,
-        }
-
-        return mnist_data, input_shapes
+    def load(self, path, **kwargs):
+        super().load(path, **kwargs)
+        self.train_set.shuffle()
+        self.test_set.shuffle()
