e98ee0c5b322bef6376b79c012b90fd15d566065
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-05-19 07:03:33 +0900

    change implement

--- visualizer/log_titanic_loss.py
+++ visualizer/log_titanic_loss.py
@@ -1,24 +1,16 @@
-from dict_keys.dataset_batch_keys import *
 from visualizer.AbstractVisualizer import AbstractVisualizer
 import numpy as np
 
 
 def get_total_acc(sess, model, next_batch_func, batch_loop, total_size):
-    acc_list = []
+    accs = []
     for i in range(batch_loop):
-        batch_xs, batch_labels = next_batch_func(
+        Xs, Ys = next_batch_func(
             model.batch_size,
-            batch_keys=[BK_X, BK_LABEL]
+            batch_keys=['Xs', 'Ys']
         )
-        acc_list += [sess.run(
-            model.batch_acc,
-            feed_dict={
-                model.X: batch_xs,
-                model.label: batch_labels,
-                model.dropout_rate: 1
-            }
-        )]
-    acc = np.concatenate(acc_list, axis=0)[:total_size]
+        accs += [model.get_tf_values(sess, model.acc, Xs=Xs, Ys=Ys)]
+    acc = np.concatenate(accs, axis=0)[:total_size]
     acc = sum(acc) / total_size
     return acc
 
@@ -27,32 +19,18 @@ class log_titanic_loss(AbstractVisualizer):
     """visualize log of classifier's loss"""
 
     def task(self, sess=None, iter_num=None, model=None, dataset=None):
-        batch_xs, batch_labels = dataset.train_set.next_batch(
+        Xs, Ys = dataset.train_set.next_batch(
             model.batch_size,
-            batch_keys=[BK_X, BK_LABEL]
+            batch_keys=['Xs', 'Ys']
         )
-        train_loss, global_step = sess.run(
-            [model.loss_mean, model.global_step],
-            feed_dict={
-                model.X: batch_xs,
-                model.label: batch_labels,
-                model.dropout_rate: 1
-            }
-        )
+        train_loss, global_step = model.get_tf_values(sess, [model.loss_mean, model.global_step], Xs, Ys)
 
         dataset.validation_set.reset_cursor()
-        batch_xs, batch_labels = dataset.validation_set.next_batch(
+        Xs, Ys = dataset.validation_set.next_batch(
             model.batch_size,
-            batch_keys=[BK_X, BK_LABEL]
+            batch_keys=['Xs', 'Ys']
         )
-        valid_loss = sess.run(
-            model.loss_mean,
-            feed_dict={
-                model.X: batch_xs,
-                model.label: batch_labels,
-                model.dropout_rate: 1
-            }
-        )
+        valid_loss = model.get_tf_values(sess, model.loss_mean, Xs, Ys)
 
         batch_loop = dataset.train_set.data_size // model.batch_size + 1
         next_batch = dataset.train_set.next_batch
