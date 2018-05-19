93a56107c5bb2179b20ec6cca42f063ca39cf835
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-05-19 21:49:30 +0900

    rename variable

--- visualizer/image_AE.py
+++ visualizer/image_AE.py
@@ -7,12 +7,12 @@ class image_AE(AbstractVisualizer):
     """visualize a tile image from GAN's result images"""
 
     def task(self, sess=None, iter_num=None, model=None, dataset=None):
-        Xs_gen, global_step = model.run(
+        Xs_recon, global_step = model.run(
             sess,
-            [model.Xs_gen, model.global_step],
+            [model.Xs_recon, model.global_step],
             dataset
         )
-        sample_imgs = np_img_float32_to_uint8(Xs_gen)
+        sample_imgs = np_img_float32_to_uint8(Xs_recon)
 
         batch_xs = dataset.train_set.next_batch(
             model.batch_size,
@@ -22,7 +22,7 @@ class image_AE(AbstractVisualizer):
         batch_xs = np_img_float32_to_uint8(batch_xs)
         sample_imgs = np.concatenate((batch_xs, sample_imgs), axis=0)
 
-        # sample_imgs = Xs_gen
+        # sample_imgs = Xs_recon
         file_name = '{}.png'.format(str(iter_num).zfill(8))
         tile = np_img_to_tile(sample_imgs, column_size=10)
         self.save_np_img(tile, file_name)
