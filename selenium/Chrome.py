726c52c318a4a5a79e5a23ae1281398c3f0d8f6b
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-02 21:23:53 +0900

    fix lint

--- selenium/Chrome.py
+++ selenium/Chrome.py
@@ -1,9 +1,5 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class Chrome:
