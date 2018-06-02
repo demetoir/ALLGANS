5018ed1143258335ac8e74f1cea8886d5441cd48
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-02 19:59:57 +0900

    update bench_code.py

--- workbench/bench_code.py
+++ workbench/bench_code.py
@@ -1,20 +1,10 @@
 # -*- coding:utf-8 -*-
-from util.Logger import StdoutOnlyLogger
-
-# print(built-in function) is not good for logging
 
 
 ########################################################################################################################
-from workbench.ppomppu import pomppu_page_crawl
+# print(built-in function) is not good for logging
 
-
-def pprint_logger(log_func):
-    def wrapper(*args, **kwargs):
-        import pprint
-        return log_func(pprint.pformat(args, **kwargs))
-
-    return wrapper
-
+from util.Logger import StdoutOnlyLogger, pprint_logger
 
 bprint = print
 logger = StdoutOnlyLogger()
@@ -26,5 +16,4 @@ pprint = pprint_logger(print)
 
 
 def main():
-    pomppu_page_crawl()
     pass
