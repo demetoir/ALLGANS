55dd81bc84c2479b51a864b421d1fb6ee0a29603
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-11 20:37:50 +0900

    better clf pack

--- sklearn_like_toolkit/ClassifierPack.py
+++ sklearn_like_toolkit/ClassifierPack.py
@@ -3,13 +3,13 @@ from pprint import pformat
 from env_settting import SKLEARN_PARAMS_SAVE_PATH
 from sklearn_like_toolkit.ParamOptimizer import ParamOptimizer
 from sklearn_like_toolkit.Base import BaseClass
-from sklearn_like_toolkit.lightGBM_wrapper import LightGBM
-from sklearn_like_toolkit.sklearn_wrapper import skMLP, skSGD, skGaussian_NB, skBernoulli_NB, skMultinomial_NB, \
+from sklearn_like_toolkit.warpper.lightGBM_wrapper import LightGBM
+from sklearn_like_toolkit.warpper.sklearn_wrapper import skMLP, skSGD, skGaussian_NB, skBernoulli_NB, skMultinomial_NB, \
     skDecisionTree, skRandomForest, skExtraTrees, skAdaBoost, skGradientBoosting, skQDA, skKNeighbors, skLinear_SVC, \
-    skRBF_SVM, skGaussianProcess
-from sklearn_like_toolkit.xgboost_wrapper import XGBoost
+    skRBF_SVM, skGaussianProcess, skVoting, skBagging
+from sklearn_like_toolkit.warpper.xgboost_wrapper import XGBoost
 from util.Logger import Logger
-from util.misc_util import path_join, time_stamp, setup_directory, dump_pickle, load_pickle
+from util.misc_util import time_stamp, dump_pickle, load_pickle
 
 
 class ClassifierPack(BaseClass):
@@ -29,40 +29,34 @@ class ClassifierPack(BaseClass):
         "skLinear_SVC": skLinear_SVC,
         "skRBF_SVM": skRBF_SVM,
         "skGaussianProcess": skGaussianProcess,
+        "skBagging": skBagging,
         "XGBoost": XGBoost,
         "LightGBM": LightGBM,
     }
 
     def __init__(self, pack_keys=None):
+        self.log = Logger(self.__class__.__name__)
         if pack_keys is None:
             pack_keys = self.class_pack.keys()
 
         self.pack = {}
         for key in pack_keys:
-            cls = self.class_pack[key]
-            obj = cls()
-            self.pack[key] = obj
+            self.pack[key] = self.class_pack[key]()
 
-        self.logger = Logger(self.__class__.__name__)
-        self.log = self.logger
+        self.optimize_result = {}
 
         self.params_save_path = SKLEARN_PARAMS_SAVE_PATH
 
     def param_search(self, Xs, Ys):
-        save_path = path_join('.', 'param_search_result', time_stamp())
-        setup_directory(save_path)
-
         for key in self.pack:
             cls = self.class_pack[key]
             obj = cls()
 
             optimizer = ParamOptimizer(obj, obj.tuning_grid)
             self.pack[key] = optimizer.optimize(Xs, Ys)
+            self.optimize_result[key] = optimizer.result
 
-            file_name = str(obj) + '.csv'
-            csv_path = path_join(save_path, file_name)
-            self.log.info("param search result csv saved at %s" % csv_path)
-            optimizer.result_to_csv(csv_path)
+            optimizer.result_to_csv()
 
             self.log.info("top 5 result")
             for result in optimizer.top_k_result():
@@ -71,27 +65,30 @@ class ClassifierPack(BaseClass):
     def predict(self, Xs):
         result = {}
         for key in self.pack:
-            clf = self.pack[key]
-            result[key] = clf.predict(Xs)
+            result[key] = self.pack[key].predict(Xs)
         return result
 
-    def fit(self, Xs, Ys, Ys_type=None):
+    def fit(self, Xs, Ys):
         for key in self.pack:
-            clf = self.pack[key]
-            clf.fit(Xs, Ys, Ys_type=Ys_type)
+            self.pack[key].fit(Xs, Ys)
 
-    def score(self, Xs, Ys, Ys_type=None):
+    def score(self, Xs, Ys):
         result = {}
         for key in self.pack:
-            clf = self.pack[key]
-            result[key] = clf.score(Xs, Ys, Ys_type=Ys_type)
+            try:
+                result[key] = self.pack[key].score(Xs, Ys)
+            except BaseException as e:
+                self.log.error(f'while score, {key} raise {e}')
+
         return result
 
-    def proba(self, Xs, transpose_shape=False):
+    def predict_proba(self, Xs):
         result = {}
         for key in self.pack:
-            clf = self.pack[key]
-            result[key] = clf.proba(Xs, transpose_shape=transpose_shape)
+            try:
+                result[key] = self.pack[key].predict_proba(Xs)
+            except BaseException as e:
+                self.log.error(f'while predict_proba, {key} raise {e}')
         return result
 
     def import_params(self, params_pack):
@@ -107,7 +104,6 @@ class ClassifierPack(BaseClass):
         return params
 
     def save_params(self, path=None):
-
         if path is None:
             path = os.path.join(self.params_save_path, time_stamp())
 
