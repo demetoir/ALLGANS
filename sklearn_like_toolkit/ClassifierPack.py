588c5a90135d253cf3bf3c7fa49b84c094a00194
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-13 05:39:53 +0900

    add score metric, add _Reformat_Ys base class

--- sklearn_like_toolkit/ClassifierPack.py
+++ sklearn_like_toolkit/ClassifierPack.py
@@ -1,24 +1,45 @@
 import os
 from pprint import pformat
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
 from env_settting import SKLEARN_PARAMS_SAVE_PATH
+from sklearn_like_toolkit.Base import BaseClass
 from sklearn_like_toolkit.FoldingHardVote import FoldingHardVote
 from sklearn_like_toolkit.ParamOptimizer import ParamOptimizer
-from sklearn_like_toolkit.Base import BaseClass
 from sklearn_like_toolkit.warpper.catboost_wrapper import CatBoostClf
 from sklearn_like_toolkit.warpper.lightGBM_wrapper import LightGBMClf
 from sklearn_like_toolkit.warpper.mlxtend_wrapper import mlxAdalineClf, mlxLogisticRegressionClf, \
-    mlxMLPClf, mlxPerceptronClf, mlxSoftmaxRegressionClf, mlxStackingCVClf, mlxStackingClf, mlxVotingClf
+    mlxMLPClf, mlxPerceptronClf, mlxSoftmaxRegressionClf, mlxStackingCVClf, mlxStackingClf
 from sklearn_like_toolkit.warpper.sklearn_wrapper import skMLP, skSGD, skGaussian_NB, skBernoulli_NB, skMultinomial_NB, \
     skDecisionTree, skRandomForest, skExtraTrees, skAdaBoost, skGradientBoosting, skQDA, skKNeighbors, skLinear_SVC, \
-    skRBF_SVM, skGaussianProcess, skVoting, skBagging
+    skRBF_SVM, skGaussianProcess, skBagging
 from sklearn_like_toolkit.warpper.xgboost_wrapper import XGBoostClf
 from util.Logger import Logger
 from util.misc_util import time_stamp, dump_pickle, load_pickle
+from util.numpy_utils import reformat_np_arr, NP_ARRAY_TYPE_INDEX, NP_ARRAY_TYPE_ONEHOT
+
+CLF_METRICS = {
+    'accuracy': accuracy_score,
+    'confusion_matrix': confusion_matrix,
+    'roc_auc_score': roc_auc_score,
+    'recall_score': recall_score,
+    'precision_score': precision_score,
+}
 
 
-# 'mlxStackingCVClf': mlxStackingCVClf,
-# 'mlxStackingClf': mlxStackingClf
-class ClassifierPack(BaseClass):
+class _Reformat_Ys:
+
+    def _reformat_to_index(self, Xs):
+        return reformat_np_arr(Xs, NP_ARRAY_TYPE_INDEX)
+
+    def _reformat_to_onehot(self, Xs):
+        return reformat_np_arr(Xs, NP_ARRAY_TYPE_ONEHOT)
+
+
+class ClassifierPack(BaseClass, _Reformat_Ys):
     class_pack = {
         "skMLP": skMLP,
         "skSGD": skSGD,
@@ -59,7 +80,10 @@ class ClassifierPack(BaseClass):
 
         self.params_save_path = SKLEARN_PARAMS_SAVE_PATH
 
+        self._metrics = CLF_METRICS
+
     def param_search(self, Xs, Ys):
+        Ys = self._reformat_to_index(Ys)
         for key in self.pack:
             cls = self.class_pack[key]
             obj = cls()
@@ -74,7 +98,7 @@ class ClassifierPack(BaseClass):
             for result in optimizer.top_k_result():
                 self.log.info(pformat(result))
 
-    def predict(self, Xs):
+    def _collect_predict(self, Xs):
         result = {}
         for key in self.pack:
             try:
@@ -83,22 +107,33 @@ class ClassifierPack(BaseClass):
                 self.log.warn(f'while fitting, {key} raise {e}')
         return result
 
+    def predict(self, Xs):
+        return self._collect_predict(Xs)
+
     def fit(self, Xs, Ys):
+        Ys = self._reformat_to_index(Ys)
         for key in self.pack:
             try:
                 self.pack[key].fit(Xs, Ys)
             except BaseException as e:
                 self.log.warn(f'while fitting, {key} raise {e}')
 
-    def score(self, Xs, Ys):
-        result = {}
-        for key in self.pack:
-            try:
-                result[key] = self.pack[key].score(Xs, Ys)
-            except BaseException as e:
-                self.log.warn(f'while score, {key} raise {e}')
+    def _apply_metric(self, Y_true, Y_predict, metric):
+        return self._metrics[metric](Y_true, Y_predict)
 
-        return result
+    def score(self, Xs, Ys, metric='accuracy'):
+        Ys = self._reformat_to_index(Ys)
+        scores = {}
+        for clf_k, predict in self._collect_predict(Xs).items():
+            scores[clf_k] = self._apply_metric(Ys, predict, metric)
+        return scores
+
+    def score_pack(self, Xs, Ys):
+        Ys = self._reformat_to_index(Ys)
+        ret = {}
+        for clf_k, predict in self._collect_predict(Xs).items():
+            ret[clf_k] = {metric: self._apply_metric(Ys, predict, metric) for metric in self._metrics}
+        return ret
 
     def predict_proba(self, Xs):
         result = {}
