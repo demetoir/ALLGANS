45897a5020573e79b454fa6e78a3d652d0a9d221
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-13 03:04:18 +0900

    add testing code

--- unit_test/test_skleanpack.py
+++ unit_test/test_skleanpack.py
@@ -1,8 +1,6 @@
 from pprint import pprint
 import numpy as np
-from data_handler.DatasetLoader import DatasetLoader
-from sklearn_like_toolkit.ClassifierPack import ClassifierPack
-from sklearn_like_toolkit.sklearn_toolkit import VotingClassifier
+import os
 
 
 def finger_print(size, head=''):
@@ -14,7 +12,19 @@ def finger_print(size, head=''):
 
 
 class test_clf_pack:
-    def __init__(self):
+
+    def setup(self):
+        print('cur dir')
+        print(os.getcwd())
+        head, tail = os.path.split(os.getcwd())
+        os.chdir(head)
+        print(os.getcwd())
+
+        from data_handler.DatasetLoader import DatasetLoader
+        from sklearn_like_toolkit.ClassifierPack import ClassifierPack
+
+        self.cls = ClassifierPack
+
         dataset = DatasetLoader().load_dataset("titanic")
         train_set, valid_set = dataset.split('train', 'train', 'valid', (7, 3))
         train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
@@ -26,7 +36,8 @@ class test_clf_pack:
         self.valid_Xs = valid_Xs
         self.valid_Ys = valid_Ys
 
-    def test_save_params(self):
+    def test_0_save_params(self):
+        ClassifierPack = self.cls
         train_Xs = self.train_Xs
         train_Ys = self.train_Ys
         valid_Xs = self.valid_Xs
@@ -39,7 +50,8 @@ class test_clf_pack:
         pprint(train_score)
         pprint(valid_score)
 
-    def test_load_params(self):
+    def test_1_load_params(self):
+        ClassifierPack = self.cls
         train_Xs = self.train_Xs
         train_Ys = self.train_Ys
         valid_Xs = self.valid_Xs
@@ -61,37 +73,108 @@ class test_clf_pack:
         pprint(train_score)
         pprint(valid_score)
 
-    def test_voting(self):
+    def test_hard_voting(self):
+        ClassifierPack = self.cls
         train_Xs = self.train_Xs
         train_Ys = self.train_Ys
         valid_Xs = self.valid_Xs
         valid_Ys = self.valid_Ys
 
-        key = 'LightGBM'
-        clf_pack = ClassifierPack()
-        pack = clf_pack.pack
+        clf = ClassifierPack()
+        clf = clf.make_FoldingHardVote()
+        clf.fit(train_Xs, train_Ys)
 
-        # pprint(pack)
-        # pprint(list(zip(*pack)))
+        predict = clf.predict(valid_Xs[:4])
+        print(f'predict {predict}')
 
-        clf_list = []
-        for i in range(3):
-            clf_list += [(k + finger_print(8, head='_'), v) for k, v in pack.items()]
+        proba = clf.predict_proba(valid_Xs[:4])
+        print(f'proba {proba}')
 
-        pprint(clf_list)
+        predict_bincount = clf.predict_bincount(valid_Xs[:4])
+        print(f'predict_bincount {predict_bincount}')
 
-        # clf_list = [(k, v) for k, v in pack.items()] * 10
-        clf = VotingClassifier(clf_list, voting='soft')
+        score = clf.score(valid_Xs, valid_Ys)
+        print(f'score {score}')
+
+    def test_stacking(self):
+        ClassifierPack = self.cls
+        train_Xs = self.train_Xs
+        train_Ys = self.train_Ys
+        valid_Xs = self.valid_Xs
+        valid_Ys = self.valid_Ys
+
+        clf = ClassifierPack()
+        metaclf = clf.pack['XGBoost']
+        clf = clf.make_stackingClf(metaclf)
+        clf.fit(train_Xs, train_Ys)
+
+        predict = clf.predict(valid_Xs[:4])
+        print(f'predict {predict}')
+
+        proba = clf.predict_proba(valid_Xs[:4])
+        print(f'proba {proba}')
+
+        score = clf.score(valid_Xs, valid_Ys)
+        print(f'score {score}')
+        pass
+
+    def test_stackingCV(self):
+        ClassifierPack = self.cls
+        train_Xs = self.train_Xs
+        train_Ys = self.train_Ys
+        valid_Xs = self.valid_Xs
+        valid_Ys = self.valid_Ys
+
+        clf = ClassifierPack()
+        metaclf = clf.pack['XGBoost']
+        clf = clf.make_stackingCVClf(metaclf)
+        clf.fit(train_Xs, train_Ys)
+
+        predict = clf.predict(valid_Xs[:4])
+        print(f'predict {predict}')
+
+        proba = clf.predict_proba(valid_Xs[:4])
+        print(f'proba {proba}')
+
+        score = clf.score(valid_Xs, valid_Ys)
+        print(f'score {score}')
+
+    def test_clf_pack_param_search(self):
+        ClassifierPack = self.cls
+        dataset = self.dataset
+        train_Xs = self.train_Xs
+        train_Ys = self.train_Ys
+        valid_Xs = self.valid_Xs
+        valid_Ys = self.valid_Ys
+
+        clf = ClassifierPack()
+        clf.param_search(train_Xs, train_Xs)
+
+        dataset.shuffle()
+        train_set, valid_set = dataset.split('train', 'train', 'valid', (7, 3))
+        train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
+        valid_Xs, valid_Ys = valid_set.full_batch(['Xs', 'Ys'])
+
+        pprint('train score', clf.score(train_Xs, train_Ys))
+        pprint('test score', clf.score(valid_Xs, valid_Ys))
+        pprint('predict', clf.predict(valid_Xs[:2]))
+        pprint('predict_proba', clf.predict_proba(valid_Xs[:2]))
+
+    def test_clfpack(self):
+        ClassifierPack = self.cls
+        dataset = self.dataset
+        train_Xs = self.train_Xs
+        train_Ys = self.train_Ys
+        valid_Xs = self.valid_Xs
+        valid_Ys = self.valid_Ys
+
+        clf = ClassifierPack()
         clf.fit(train_Xs, train_Ys)
         score = clf.score(valid_Xs, valid_Ys)
-        pprint(score)
+        pprint('test score', score)
+        predict = clf.predict(valid_Xs[:2])
+        pprint('predict', predict)
+        proba = clf.predict_proba(valid_Xs[:2])
+        pprint('predict_proba', proba)
 
-    def run(self):
-        self.test_load_params()
-        self.test_save_params()
-        self.test_voting()
-
-
-if __name__ == '__main__':
-    test_class = test_clf_pack()
-    test_class.run()
+        # initialize data
